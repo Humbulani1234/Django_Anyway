ed687a5aacc9de51293e587b3cb6448179613a67
humbulani <ndousmall@gmail.com>
2023-04-20 12:32:17 +0200

    packages

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,9 +129,9 @@ STATIC_URL = 'static/'
 #STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_ROOT = BASE_DIR / 'static'
 
-#STATICFILES_DIRS = [
-#    BASE_DIR / 'static/'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles'
+ ]
 
 
 # Default primary key field type
