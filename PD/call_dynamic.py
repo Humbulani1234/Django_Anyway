e337351211442c5107e575de5a2352b08eb528d0
Humbulani1234 <92155443+Humbulani1234@users.noreply.github.com>
2023-08-22 17:06:26 +0200

    Delete PD directory

--- PD/call_dynamic.py
+++
@@ -1,8 +0,0 @@
-from pycallgraph2 import PyCallGraph
-from pycallgraph2.output import GraphvizOutput
-
-graph = GraphvizOutput()
-graph.output_file = "file4.png"
-
-with PyCallGraph(output=graph):
-  print("Hello world")
